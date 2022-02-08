60c02fbe226dc5c664468e36282e13697e6c059c
Alexandre Palota <alexandre.palota@gmail.com>
2022-02-08 13:15:32 -0300

    Typography

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Notes from './pages/Notes'
+import Create from './pages/Create'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Create.jsx
@@ -0,0 +1,19 @@
+import Typography from '@material-ui/core/Typography';
+
+export default function Create() {
+    return (
+        <div>
+            <Typography
+                variant='h1'
+                color='primary'
+                align='center'>
+                Create a New Note
+            </Typography>
+            <Typography
+                color='secondary'
+                noWrap>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus cum optio quasi consectetur molestiae nam minima impedit, quibusdam dolorum. Et alias maxime aspernatur excepturi iure similique praesentium? Consequuntur, ipsum consectetur.
+            </Typography>
+        </div>
+    )
+}
