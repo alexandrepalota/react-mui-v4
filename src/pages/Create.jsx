e2cb90bcb8d214d75749315e999fd049cce8cc86
Alexandre Palota <alexandre.palota@gmail.com>
2022-02-08 15:18:43 -0300

    makeStyles Hook

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,31 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    btn: {
+        fontSize: 60,
+        backgroundColor: 'violet',
+        '&:hover': {
+            backgroundColor: 'blue'
+        }
+    },
+    title: {
+        textDecoration: 'underline',
+        marginBottom: 20
+    }
+})
 
 export default function Create() {
+
+    const classes = useStyles()
+
     return (
         <Container>
             <Typography
+                className={classes.title}
                 variant='h6'
                 component='h2'
                 color='textSecondary'
@@ -15,9 +35,11 @@ export default function Create() {
             </Typography>
 
             <Button
+                className={classes.btn}
                 onClick={() => console.log('You clicked me')}
                 color='secondary'
                 variant='contained'
+                endIcon={<KeyboardArrowRightIcon />}
             >
                 Submit
             </Button>
