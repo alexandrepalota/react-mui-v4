87a71aba2a2dcc0985f277e92d35cd6912e37c8b
Alexandre Palota <alexandre.palota@gmail.com>
2022-02-08 21:34:09 -0300

    Menu lateral

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,40 @@
 import { makeStyles } from "@material-ui/core"
+import { Drawer, Typography } from "@material-ui/core"
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
+    root: {
+        display: 'flex'
+    },
     page: {
         background: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
     }
 })
 
 export default function Layout({ children }) {
     const classes = useStyles()
     return (
-        <div>
+        <div className={classes.root}>
+            <Drawer
+                className={classes.drawer}
+                variant='permanent'
+                anchor='left'
+                classes={{paper: classes.drawerPaper}}
+            >
+                <div>
+                    <Typography variant='h5'>
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
             <div className={classes.page}>
                 {children}
             </div>
